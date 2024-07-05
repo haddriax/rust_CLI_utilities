a135158e023e1689b6181552605ecae0464a2d92
gael <hadriaxdev@proton.me>
2024-07-05 15:34:48 +0200

    Added functions documentations.

--- cli_utilities/src/main.rs
+++ cli_utilities/src/main.rs
@@ -2,6 +2,26 @@ use std::io::{self, BufRead, BufReader};
 use std::fs::File;
 use std::path::Path;
 
+/// Reads a line of user input from the standard input.
+///
+/// # Returns
+///
+/// `Ok(String)` - The line of user input, without trailing newline characters.
+/// `Err(io::Error)` - If there is an error reading from the standard input.
+///
+/// # Examples
+///
+/// ```
+/// use std::io;
+///
+/// fn main() {
+///     let result = get_user_input();
+///     match result {
+///         Ok(input) => println!("User input: {}", input),
+///         Err(error) => eprintln!("Error: {}", error),
+///     }
+/// }
+/// ```
 fn get_user_input() -> io::Result<String> {
     let mut input = String::new();
     io::stdin().read_line(&mut input)?;
@@ -9,6 +29,15 @@ fn get_user_input() -> io::Result<String> {
     Ok(input.trim().to_owned())
 }
 
+/// Prints the content of a file line by line.
+///
+/// # Arguments
+///
+/// * `path` - A reference to a `Path` object that represents the path of the file.
+///
+/// # Returns
+///
+/// Returns an `io::Result` indicating whether the operation was successful or not.
 fn print_file_content_by_line<P: AsRef<Path>>(path: P) -> io::Result<()> {
     let file = File::open(&path)?;
     let reader = BufReader::new(file);
@@ -23,6 +52,13 @@ fn print_file_content_by_line<P: AsRef<Path>>(path: P) -> io::Result<()> {
     Ok(())
 }
 
+/// Continuously prompts the user for a file path and echoes the content of the file by line.
+///
+/// The user can exit the loop by entering "exit". The function keeps looping until the user exits.
+///
+/// # Panics
+///
+/// This function panics if it fails to read from standard input, or if there is an error while opening the file.
 fn user_loop() {
     loop {
         println!("{}", "Path of the file to echo:");
